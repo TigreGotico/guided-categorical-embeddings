e87cf8dfbfa99d66c0e6dc0c093f6d357fd956a4
miro <jarbasai@mailfence.com>
2024-10-25 21:23:04 +0100

    feat:semver

--- setup.py
+++ setup.py
@@ -1,12 +1,69 @@
+#!/usr/bin/env python3
+import os
+
 from setuptools import setup
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+with open("README.md", "r") as fh:
+    long_desc = fh.read()
+
+
+def get_version():
+    """ Find the version of the package"""
+    version = None
+    version_file = os.path.join(BASEDIR, 'guided_categorical_embeddings', 'version.py')
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if alpha:
+        version += f"a{alpha}"
+    return version
+
+
+def required(requirements_file):
+    """ Read requirements file and remove comments and empty lines. """
+    with open(os.path.join(BASEDIR, requirements_file), 'r') as f:
+        requirements = f.read().splitlines()
+        if 'MYCROFT_LOOSE_REQUIREMENTS' in os.environ:
+            print('USING LOOSE REQUIREMENTS!')
+            requirements = [r.replace('==', '>=').replace('~=', '>=') for r in requirements]
+        return [pkg for pkg in requirements
+                if pkg.strip() and not pkg.startswith("#")]
+
+
+def package_files(directory):
+    paths = []
+    for (path, _, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
 
 setup(
     name='guided-categorical-embeddings',
-    version='0.0.1',
+    version=get_version(),
     packages=['guided_categorical_embeddings'],
+    package_data={'': package_files('guided_categorical_embeddings')},
     url='',
     license='',
     author='JarbasAI',
     author_email='jarbasai@mailfence.com',
-    description=''
+    description='',
+    install_requires=required('requirements.txt'),
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
 )
